23e61361e8f5a67a800627452dc9a661db261cbf
mr-fact <mr.fact.202020@gmail.com>
2024-12-13 14:14:40 +0330

    add addmin user to User Seeder

--- sokancoin/database/seeders/UserSeeder.php
+++ sokancoin/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gmmail.com',
+            'password'=> Hash::make('admin_password'),
+        ]);
         User::factory()->count(count: 50)->create();
     }
 }
