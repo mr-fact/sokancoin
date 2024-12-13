d622eb7066db0c82d28286529d6eb39c50d86616
mr-fact <mr.fact.202020@gmail.com>
2024-12-13 12:59:32 +0330

    create transaction model

---
+++ sokancoin/database/migrations/2024_12_13_090058_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('origin_wallet_id')->nullable()->constrained('wallets')->onDelete('set null');
+            $table->foreignId('destination_wallet_id')->nullable()->constrained('wallets')->onDelete('set null');
+            $table->boolean('status')->default(false);
+            $table->bigInteger('amount')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
