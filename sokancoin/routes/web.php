8d2400b1eeab4e155e58865b69cc409c8582c3d6
mr-fact <mr.fact.202020@gmail.com>
2024-12-13 19:52:34 +0330

    redirect root url to wallets

--- sokancoin/routes/web.php
+++ sokancoin/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransactionController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/wallets');
 });
 
 require __DIR__.'/transactions.php';
